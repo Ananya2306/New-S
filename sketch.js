2e02f8bb8a367978db30513bb85257b36d723adf
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-06-26 22:41:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var canvas, backgroundImage;
+// create variables
+var canvas;
 var gameState = 0;
 var playerCount;
 var database;
@@ -6,18 +7,22 @@ var form, player, game;
 var allPlayers;
 
 function setup(){
+    // create canvas
     canvas = createCanvas(displayWidth - 20, displayHeight-30);
+    // add databse 
     database = firebase.database();
+    // make new game 
     game = new Game();
     game.getState();
     game.start();
 }
 
 function draw(){
+    // for updating the game on Play state
     if(playerCount === 2){
         game.update(1);
     }
-
+// Game state play 
     if(gameState===1){
         clear();
         game.play();
