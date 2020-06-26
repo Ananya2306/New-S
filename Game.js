438cfaf91240fe751af3e4ebd148be3a184c7bc3
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-06-26 22:23:15 +0530

    Update Game.js

--- Game.js
+++ Game.js
@@ -3,6 +3,7 @@ class Game {
 
     }
     getState(){
+        // Read the database for gameState 
         var gameStateRef = database.ref("gameState");
         gameStateRef.on("value", function(data){
             gameState = data.val();
@@ -10,12 +11,14 @@ class Game {
     }
 
     update(state){
+        //Give the state
         database.ref("/").update({
             gameState: state
         })
     }
-
+// make a start function
     async start(){
+        // Wait for the other person to login 
         if(gameState===0){
             player = new Player();
             var playerCountRef = await database.ref("playerCount").once("value");
@@ -27,9 +30,11 @@ class Game {
             form.display();
         }
     }
-
+//Create the play function  as gameState Play
     play(){
+        // Hide the Form
         form.hide();
+        // Give the text to know the person that Game is started
         textSize(40);
         text("Start", 620, 100);
         Player.getPlayerinfo();
@@ -38,17 +43,18 @@ class Game {
             var display_position = 130;
             for(var plr in allPlayers){
                 if(plr==="player" + player.index)
-                    fill("blue");
+                    fill("red");
 
                 else
                     fill("black");
 
                 display_position+=40;
+                // Diplay the name of the person by red color 
                 textSize(25);
                 text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 620,display_position);
             }
         }
-
+        
         if(keyDown(UP_ARROW) && player.index!==null){
             player.distance+=50;
             player.update();
